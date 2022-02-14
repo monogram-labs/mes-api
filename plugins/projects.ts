8ef348ee09ef4c8cf9a529fe07a7a4b0cfb06f5d
Claudio F <claudio@monogram.io>
2022-02-14 12:40:38 -0500

    using GET for project details

--- plugins/projects.ts
+++ plugins/projects.ts
@@ -1,5 +1,5 @@
 import Hapi from '@hapi/hapi'
-import { Prisma } from '@prisma/client'
+// import { Prisma } from '@prisma/client'
 /*
  * TODO: We can't use this type because it is available only in 2.11.0 and previous versions
  * In 2.12.0, this will be namespaced under Prisma and can be used as Prisma.UserCreateInput
@@ -14,7 +14,7 @@ const projectsPlugin = {
 	register: async function (server: Hapi.Server) {
 		server.route([
 			{
-				method: 'POST',
+				method: 'GET',
 				path: '/project',
 				handler: getProjectDetailsHandler
 			}
@@ -50,25 +50,33 @@ async function newProjectHandler(request: Hapi.Request, h: Hapi.ResponseToolkit)
 
 async function getProjectDetailsHandler(request: Hapi.Request, h: Hapi.ResponseToolkit) {
 	const { prisma } = request.server.app
-	const { apiKey, projectId } = request.payload as any
+	const query = request.query as any
 
-	console.log('Authorization:', request.headers.authorization)
-	console.log('apiKey', apiKey, 'projectId', projectId)
+	const projectId = query?.projectId
+	const apiKey = request.headers['x-api-key']
 
 	// return 400 if apiKey or projectId is missing
-	if (!apiKey || !projectId) return h.response({ message: 'Missing apiKey or projectId' }).code(400)
+	if (!apiKey || !projectId)
+		return h.response({ message: 'Missing API key or projectId.' }).code(400)
 
 	try {
-		const project = await prisma.project.findUnique({
+		const project = await prisma.project.findMany({
 			where: {
-				id: projectId
+				AND: {
+					id: projectId,
+					apiKeys: {
+						some: {
+							key: apiKey
+						}
+					}
+				}
 			},
 			include: {
 				apiKeys: true
 			}
 		})
 
-		return project ? h.response(project).code(200) : null
+		return Array.isArray(project) && project.length > 0 ? h.response(project[0]).code(200) : null
 	} catch (err) {
 		console.log(err)
 	}
